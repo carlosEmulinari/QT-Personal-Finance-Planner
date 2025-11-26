d0a254d73e2b01ef19400e98ce3dd0bb675d6e3c
carlosEmulinari <carlosmulinari10@gmail.com>
2025-11-26 17:04:46 -0300

    Update mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,4 +12,48 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+// ---------------- Button Slot Functions ----------------
+
+void MainWindow::on_pushButton_clicked()
+{
+    openAccountSection();
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    openSavingSection();
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    openInvestmentSection();
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    openConsultantSection();
+}
+
+// ---------------- Custom Internal Functions ----------------
+
+void MainWindow::openAccountSection()
+{
+    QMessageBox::information(this, "Account", "Opening Account Section...");
+}
+
+void MainWindow::openSavingSection()
+{
+    QMessageBox::information(this, "Saving", "Opening Saving Section...");
+}
+
+void MainWindow::openInvestmentSection()
+{
+    QMessageBox::information(this, "Investments", "Opening Investment Section...");
+}
+
+void MainWindow::openConsultantSection()
+{
+    QMessageBox::information(this, "Consultant", "Opening Consultant Section...");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,9 +4,7 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -14,10 +12,23 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();      // Account button
+    void on_pushButton_2_clicked();    // Saving button
+    void on_pushButton_3_clicked();    // Investments button
+    void on_pushButton_4_clicked();    // Consultant button
 
 private:
     Ui::MainWindow *ui;
+
+    // --- Custom internal functions (no extra files required) ---
+    void openAccountSection();
+    void openSavingSection();
+    void openInvestmentSection();
+    void openConsultantSection();
 };
+
 #endif // MAINWINDOW_H
